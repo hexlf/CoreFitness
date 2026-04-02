b8d1cb827f8edcfc7813527fe96d3323ba87ed51
ZelFS <86790952+hexlf@users.noreply.github.com>
2026-04-02 14:26:36 +0200

    add SignUp and start on footer and header

---
+++ CoreFitness/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
