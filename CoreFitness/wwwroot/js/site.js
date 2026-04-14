bafe298c920f8691f2b7ba90f0ef477b3f82e74d
ZelFS <86790952+hexlf@users.noreply.github.com>
2026-04-14 11:45:48 +0200

    add fixed Navbar and add JS for onclick

--- CoreFitness/wwwroot/js/site.js
+++ CoreFitness/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿
+﻿function toggleDropdown() {
+    const dropdown = document.getElementById('trainingDropdown');
+    dropdown.classList.toggle('open');
+}    
+//Close dropdown onclick
+document.addEventListener('click', function (e) {
+    const dropdown = document.getElementById('trainingDropdown');
+    if (dropdown && !dropdown.contains(e.target)) {
+        dropdown.classList.remove('open');
+    }
+});
